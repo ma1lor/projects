39b099a2a9ed158fa8eef73c507153970bb5e257
ma1lor <102816803+ma1lor@users.noreply.github.com>
2022-08-02 21:18:22 +0300

    Add files via upload

---
+++ 2project.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+
+
+int rockscissorsPaper(){
+    // rock = 1 scissorss = 2 paper = 3
+    srand(time(0));
+    int a = rand() % 3 + 1;
+    return a;
+}
+
+int main(){
+    int userTool;
+    std::cout<< "take ur tool(rock = 1 scissors = 2 paper = 3): ";
+    std::cin >> userTool;
+
+    int computersTool = rockscissorsPaper();
+    std::cout<< "computer's tool: " << computersTool<<std::endl;
+    
+    
+    switch(computersTool){
+        case(1):
+            if(userTool == 2){
+                std::cout << "ure lost!";
+                break;
+            }
+            else if(userTool == 1){
+                std::cout << "draw";
+                break;
+
+            }
+            else {
+                std::cout << "ure won";
+                break;
+            }
+        case(2):
+            if(userTool == 3){
+                std::cout << "ure lost!";
+                break;
+            }
+            else if(userTool == 2){
+                std::cout << "draw";
+                break;
+
+            }
+            else {
+                std::cout << "ure won";
+                break;
+            }
+        case(3):
+            if(userTool == 1){
+                std::cout << "ure lost!";
+                break;
+            }
+            else if(userTool == 3){
+                std::cout << "draw";
+                break;
+
+            }
+            else {
+                std::cout << "ure won";
+                break;
+            }
+        
+    }
+}
